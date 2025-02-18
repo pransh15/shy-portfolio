79d0866eb2bb93bc6d8e7e61d289ebc9be565576
Ariel Falcón <ariel.falcon@zenerconsulting.com>
2025-02-18 22:55:00 +0100

    vercel debug

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,4 @@
-import contextCursor from "../lib/context-cursor/contextCursor";
+import contextCursor from "@/lib/contextCursor";
 
 var mq = window.matchMedia("(min-width: 640px)");
 if (mq.matches) {
